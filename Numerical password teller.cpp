3d646d7676c66d7ef90492b18328e63680c38e0d
Muhammad Shahmir Khan <sshhaahhmmiirr@gmail.com>
2020-02-17 10:31:58 +0500

    CODE

---
+++ Numerical password teller.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main(){
+	int a,b;
+	cout<<"\n\n\n\t\t*******    NUMARICAL PASSWORD TELLER   *******\n\n\n";
+	cout<<"    ENTER YOUR PASSWORD  : ";
+	cin>>a;
+	 int i=0;
+	for(int o=0;o<=a;o++){
+
+	
+	 b=i;
+	 i++;
+}//for
+	 
+	if(a==b){
+		cout<<"\n\n\t    *******   PASSWORD CRACKED   *******\n\n\n";
+		cout<<"    PASSWORD IS : "<<a;
+		}//if
+	
+	getch();
+}
